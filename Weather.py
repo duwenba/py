57f18137141b267e036aebaff944f13cd8457425
duwenba <2275317692@qq.com>
2022-12-06 09:59:43 +0800

    2022-12-6 09:59:40

--- Weather.py
+++ Weather.py
@@ -6,12 +6,3 @@ from PyQt6.QtWidgets import QApplication, QFrame
 # from PyQt6 import QtCore,QtGui
 from PyQt6.QtCore import Qt
 
-app = QApplication(sys.argv)
-fram =QFrame()
-fram.setAttribute(Qt.WidgetAttribute.WA_TranslucentBackground)#WA_TranslucentBackground)  # 窗体背景透明
-fram.setWindowFlags(Qt.WindowType.WindowStaysOnTopHint | Qt.WindowType.FramelessWindowHint | Qt.WindowType.Tool)  #窗口置顶，无边框，在任务栏不显示图标
-# fram.setWindowFlags(Qt.WindowType.FramelessWindowHint)#(PyQtWindowMinMaxButtonsHint | Qt::FramelessWindowHint)
-ui = ui_untitled.Ui_Frame()
-ui.setupUi(fram)
-fram.show()
-sys.exit(app.exec())
